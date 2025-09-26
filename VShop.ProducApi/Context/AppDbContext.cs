00c7765f649645436a197cb9c06d0d231617e4fb
valentimhelio <valentimhelio@gmail.com>
2025-09-26 14:28:46 -0400

    280. Criando o projeto no Visual Studio

---
+++ VShop.ProducApi/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProducApi.Models;
+
+namespace VShop.ProducApi.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Category> Categores { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ VShop.ProducApi/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace VShop.ProducApi.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = String.Empty;
+    public long Stock {  get; set; }
+    public string ImageURL { get; set; } = String.Empty;
+
+    public Category category { get; set; }
+    public int CategoryId { get; set; }
+}
